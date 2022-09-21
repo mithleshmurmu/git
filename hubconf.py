5d79fbe6e71807c89714069949ecbf4e078f0af2
MITHLESH MURMU <30808537+mithleshmurmu@users.noreply.github.com>
2022-09-21 12:44:56 +0530

    update optimizer

--- hubconf.py
+++ hubconf.py
@@ -76,7 +76,7 @@ def get_model():
 
 #################################
 
-optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+optimizer = torch.optim.SGD(get_model().parameters(), lr=1e-3)
 
 ####################################
 
